4e029c0f2fbe45e936909acd1a689cb52239aa94
krish <krish@LAPTOP-4KTRC8TA>
2025-09-26 21:59:41 +0530

    this is my second commit

---
+++ vtiger-crm-fw-a12/src/test/java/ddt_extra/GetDataFromPropertiesFile.java
@@ -0,0 +1,24 @@
+package ddt_extra;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class GetDataFromPropertiesFile {
+	public static void main(String[] args) throws IOException {
+		FileInputStream fis = new FileInputStream("./src/test/resources/CommonData.properties");
+		// ./src/test/resources/CommonData.properties
+		Properties pObj = new Properties();
+		pObj.load(fis);
+
+		String BROWSER = pObj.getProperty("bro");
+		String URL = pObj.getProperty("url");
+		String USERNAME = pObj.getProperty("un");
+		String PASSWORD = pObj.getProperty("pwd");
+
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+	}
+}
